5c77c2604597d53c599a9d1781d017f701e71b24
Sungmin <kangsm0903@naver.com>
2026-04-11 10:28:26 +0900

    Recursion

--- Main.java
+++ Main.java
@@ -1,46 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main{
+    public static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
+        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+        sb.append("\"재귀함수가 뭔가요?\"\n");
+        sb.append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+        sb.append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+        sb.append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
 
-        int result = func1(r, c, N);
-        System.out.println(result);
+        func1(N, 1);
+
+        sb.append("라고 답변하였지.");
+
+        System.out.println(sb);
     }
 
-    public static int func1(int row, int col, int N){
-        if(N==1){ // 00=0, 01=1, 10=2, 11=3
-            if(row==0 & col==0){
-                return 0;
-            } else if(row==0 && col==1){
-                return 1;
-            } else if (row==1 && col==0){
-                return 2;
-            } else if (row==1 && col==1){
-                return 3;
-            }
-        }
-
-        int base = (int)Math.pow(2, N)/2;
-        if(row < base && col < base){ // 1
-            return func1(row, col, N-1);
-        } else if(row < base && col >= base){ // 2
-            return func1(row, col - base, N-1) + (base*base);
-        } else if(row >= base && col < base){ // 3
-            return func1(row - base, col, N-1) + 2*(base*base);
-        } else if(row >= base && col >= base){ // 4
-            return func1(row - base, col - base, N-1) + 3*(base*base);
+    public static void func1(int N, int cnt){
+        if(N==1){
+            sb.append("_".repeat(cnt*4)).append("\"재귀함수가 뭔가요?\"\n");
+            sb.append("_".repeat(cnt*4)).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+            sb.append("_".repeat(cnt*4)).append("라고 답변하였지.\n");
         } else {
-            return -1;
+            sb.append("_".repeat(cnt*4)).append("\"재귀함수가 뭔가요?\"\n");
+            sb.append("_".repeat(cnt*4)).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+            sb.append("_".repeat(cnt*4)).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+            sb.append("_".repeat(cnt*4)).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+
+            func1(N-1, cnt+1);
+
+            sb.append("_".repeat(cnt*4)).append("라고 답변하였지.\n");
         }
-    }
+
+
+    } 
 }
